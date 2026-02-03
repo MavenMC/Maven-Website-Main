194a57f6865727983d0775bcd1ae519111efad27
Ruimmp <mug.carburant_0u@icloud.com>
2026-02-03 01:22:20 +0100

    feat: redirect store and resolve hydration issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,16 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/logo.png" />
       </head>
-      <body className="bg-[#0b0f16] text-white">
+      <body className="bg-[#0b0f16] text-white" suppressHydrationWarning>
         <Navbar />
         <main className="p-8">{children}</main>
       </body>
     </html>
   );
-
-
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,21 +6,14 @@ export default function Header() {
   return (
     <header className="w-full bg-[#16090B]/90 backdrop-blur border-b border-white/10">
       <div className="max-w-7xl mx-auto px-6 h-16 flex items-center">
-        
         {/* ESQUERDA */}
         <nav className="flex items-center gap-8 text-base font-semibold">
-          <Link
-            href="/"
-            className="flex items-center gap-2 text-white hover:text-red-500 transition"
-          >
+          <Link href="/" className="flex items-center gap-2 text-white hover:text-red-500 transition">
             <Home size={20} />
             Início
           </Link>
 
-          <Link
-            href="/validar"
-            className="flex items-center gap-2 text-white/80 hover:text-white transition"
-          >
+          <Link href="/validar" className="flex items-center gap-2 text-white/80 hover:text-white transition">
             <Gamepad2 size={20} />
             Jogar
           </Link>
@@ -32,7 +25,9 @@ export default function Header() {
         {/* DIREITA */}
         <div className="flex items-center gap-6">
           <Link
-            href="/loja"
+            href="https://loja.mavenmc.com.br/"
+            target="_blank"
+            rel="noopener noreferrer"
             className="
               flex items-center gap-2
               bg-red-600 hover:bg-red-700
